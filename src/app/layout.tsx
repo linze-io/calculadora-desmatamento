4cd2f7a65edc92b7df24edd312e3b9b392d65ab7
Keven Jesus <falecom@keven.com.br>
2023-05-30 19:22:24 -0300

    teste

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css"
 import { Inter } from "next/font/google"
 
 const inter = Inter({ subsets: ["latin"] })
-const foo = ""
 
 export const metadata = {
   title: "Create Next App",
