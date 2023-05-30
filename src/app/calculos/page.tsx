ef0db00dce29d1f26a0cb65f8ac50ea4684a85b4
Keven Jesus <falecom@keven.com.br>
2023-05-30 19:57:43 -0300

    teste

--- src/app/calculos/page.tsx
+++ src/app/calculos/page.tsx
@@ -4,10 +4,11 @@ import { useEffect } from "react"
 import woodAndNonWoodProducts from "../../calculations/useCalculator/useDesforestation/woodAndNonWoodProducts"
 
 export default function Calculos() {
+  const test = "valor"
   useEffect(() => {
     const woodAndNonWoodProducts1 = woodAndNonWoodProducts()
 
     console.log("TotalwoodAndNonWoodProducts", woodAndNonWoodProducts1)
   }, [])
-  return <h2>teste</h2>
+  return <h2>teste {test}</h2>
 }
