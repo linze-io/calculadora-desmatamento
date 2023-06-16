c5e0c1e9eb405249e755f1cd50acc7a247ad0b9f
caduveloso <72165203+caduveloso@users.noreply.github.com>
2023-06-16 09:21:34 -0300

    cleaning the code

--- src/app/components/Calculator.js
+++ src/app/components/Calculator.js
@@ -10,7 +10,6 @@ import Typography from "@mui/material/Typography"
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 import TextField from "@mui/material/TextField"
 
-
 export default function Home() {
   return (
     <>
